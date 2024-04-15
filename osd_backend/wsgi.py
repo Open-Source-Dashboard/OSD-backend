dcd4c3e66e6e48afb957b8cbec6da72b3692a3d0
tammytdo11 <tammytdo11@gmail.com>
2024-04-15 10:06:59 -0700

    recreated django setup to resolve naming issues

--- osd_backend/wsgi.py
+++ osd_backend/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for OCD_backend project.
+WSGI config for osd_backend project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OCD_backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'osd_backend.settings')
 
 application = get_wsgi_application()
---
+++ osd_dashboard_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class OsdDashboardAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'osd_dashboard_app'
