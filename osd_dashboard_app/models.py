08d0c7dc80d6a73ac15c546eb8744db79135020c
tammytdo11 <tammytdo11@gmail.com>
2024-04-16 00:27:43 -0700

    set up osd_dashboard_app

--- osd_dashboard_app/models.py
+++ osd_dashboard_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+'''
+User model that includes
+
+# User name
+# Donut stampcard
+# Donut boxes
+# Last commit date
+# Last commit repo name
+'''
+
+
+'''
+Repo model that includes
+# name
+# license
+# avatar_url 
+# url 
+# commits_url 
+# topics 
+# latest_commit_timestamp 
+# latest_committer 
+'''
