bd0c3ab3bbf24d3fbcae7c1652760245d8c38d2a
lana-z <lana@levelupeconomy.com>
2024-04-23 09:41:40 -0700

    vercel settings

--- osd_dashboard_app/urls.py
+++ osd_dashboard_app/urls.py
@@ -4,6 +4,6 @@ from osd_dashboard_app.views import GitHubRepositoriesView
 from .auth import GitHubAuthCallback 
 
 urlpatterns = [
-  path('repositories/', GitHubRepositoriesView.as_view()),
+  path('repos/', GitHubRepositoriesView.as_view()),
   path('github/callback/', GitHubAuthCallback.as_view(), name='github_callback'),
 ]
