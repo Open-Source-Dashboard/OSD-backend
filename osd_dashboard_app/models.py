11ad92594d7ef3fadce479a2f166767e32e8f0b4
Andrea Riley <andreariley@me.com>
2024-04-30 10:18:46 -0400

    smalkl rename

--- osd_dashboard_app/models.py
+++ osd_dashboard_app/models.py
@@ -4,6 +4,9 @@ from django.utils import timezone
 from datetime import datetime, timedelta
 import requests
 
+
+
+
 class GithubUser(models.Model):
     username = models.CharField(max_length=100)
     avatar_url = models.URLField()
@@ -25,22 +28,18 @@ class GithubRepo(models.Model):
     
     def __str__(self):
         return self.name
-    
+
 # does views latest_contributors logic need to go here?
-class Contributor(models.Model):
+class RepoContributor(models.Model):
     name = models.CharField(max_length=255)
     repo = models.ForeignKey(GithubRepo, on_delete=models.CASCADE)
     commit_url = models.URLField()
     last_commit_repo_name = models.CharField(max_length=255)
 
 
-# from django.contrib.auth.models import AbstractUser
-# import requests
-
-# class User(AbstractUser):
-#     github_username = models.CharField(max_length=100, blank=True, null=True)
-#     donut_stampcard_count = models.IntegerField(default=0)
-#     donut_boxes = models.IntegerField(default=0)
-#     last_commit_date = models.DateTimeField(null=True, blank=True)
-#     last_commit_repo_name = models.CharField(max_length=255, null=True, blank=True)
-
+class OSDuser(AbstractUser):
+    github_username = models.CharField(max_length=100, blank=True, null=True)
+    donut_stampcard_count = models.IntegerField(default=0)
+    donut_boxes = models.IntegerField(default=0)
+    last_commit_date = models.DateTimeField(null=True, blank=True)
+    last_commit_repo_name = models.CharField(max_length=255, null=True, blank=True)
