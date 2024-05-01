eff217c5c7fa4dee2bb07749b870fa88cf6e4cc9
Andrea Riley <andreariley@me.com>
2024-05-01 18:17:00 -0400

    the views went on a diet

--- osd_dashboard_app/views.py
+++ osd_dashboard_app/views.py
@@ -13,5 +13,13 @@ env = environ.Env(DEBUG=(bool, False))
 environ.Env.read_env(raise_error_if_not_found=True)
 
 class GitHubRepositoriesView(View):
-
+    template_name = 'repositories.html'
   
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        all_repos = GithubRepo.objects.fetch_repos()
+        context['popular_repos'] = GithubRepo.objects.get_popular_repos(all_repos)
+        context['featured_repo'] = GithubRepo.objects.get_featured_repo(all_repos)
+        context['hacktoberfest_repos'] = GithubRepo.objects.prioritize_hacktoberfest_repos(all_repos)
+        
+        return context
