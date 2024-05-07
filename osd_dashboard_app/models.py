00662f3bf7563cd5260690faf609e4e1e58f442c
Andrea Riley <andreariley@me.com>
2024-05-07 17:16:37 -0400

    refactor done

--- osd_dashboard_app/models.py
+++ osd_dashboard_app/models.py
@@ -8,46 +8,6 @@ import requests, random, environ
 env = environ.Env()
 environ.Env.read_env()
 
-class GithubRepoManager(models.Manager):
-    """Fetch and process repositories from GitHub."""
-    def fetch_repos(self, include_hacktoberfest=False):
-        last_month = (datetime.now() - timedelta(days=30)).strftime("%Y-%m-%dT%H:%M:%SZ")
-        url = 'https://api.github.com/search/repositories'
-        query = 'opensource'
-        if include_hacktoberfest:
-            query += ' hacktoberfest'
-        headers = {"Authorization": f"Bearer {env('GITHUB_ORG_ACCESS_TOKEN')}"}
-
-        params = {
-            "q": f"topic:{query} pushed:>{last_month}",
-            "order": "desc",
-            "sort": "updated",
-            "per_page": 20
-        }
-        try:
-            response = requests.get(url,headers=headers, params=params)
-            response.raise_for_status()
-            return response.json().get('items', [])
-        except requests.exceptions.RequestException as e:
-            print(f'Error fetching repositories: {str(e)}')
-            return []
-
-    def get_popular_repos(self, repositories):
-        """Sort repositories by stargazers_count in descending order."""
-        return sorted(repositories, key=lambda x: x['stargazers_count'], reverse=True)
-
-    # double check randomizer
-    def get_featured_repo(self, repositories):
-        """Select a single random repository from a list to feature."""
-        return random.choice(repositories) if repositories else None
-
-    def prioritize_hacktoberfest_repos(self, repositories):
-        hacktoberfest_repos = [repo for repo in repositories if 'hacktoberfest' in repo.get('topics', [])]
-        other_repos = [repo for repo in repositories if 'hacktoberfest' not in repo.get('topics', [])]
-        return hacktoberfest_repos + other_repos
-
-
-# If performance becomes an issue, consider alternative methods to achieve randomness, such as selecting a random index in Python and retrieving the specific entry by ID.
 
 class GithubUser(models.Model):
     username = models.CharField(max_length=100)
@@ -56,6 +16,80 @@ class GithubUser(models.Model):
 
     def __str__(self):
         return self.username
+
+
+class GithubRepoManager(models.Manager):
+    """Fetch and process repositories from GitHub."""
+
+    def fetch_repos(self, include_hacktoberfest=False):
+        last_month = (datetime.now() - timedelta(days=30)).strftime("%Y-%m-%dT%H:%M:%SZ")
+        url = 'https://api.github.com/search/repositories'
+        headers = {"Authorization": f"Bearer {env('GITHUB_ORG_ACCESS_TOKEN')}"}
+
+        params = {
+            "q": f"topic:opensource hacktoberfest pushed:>{last_month}",
+            "order": "desc",
+            "sort": "updated",
+            "per_page": 20
+        }
+
+        try:
+            response = requests.get(url,headers=headers, params=params)
+            response.raise_for_status()
+            repositories = response.json()['items']
+            return repositories
+        except requests.exceptions.RequestException as e:
+            print(f'Error fetching repositories: {str(e)}')
+
+    def get_popular_repos(self, repositories):
+        """Sort repositories by stargazers_count in descending order."""
+        
+        return random.sample(repositories, min(len(repositories), 5))
+    # double check randomizer
+    def get_featured_repo(self, repositories):
+        """Select a single random repository from a list to feature."""
+        
+        featured_repo = random.choice(repositories) if repositories else None
+        print(f'featured repo: {featured_repo}')
+        return featured_repo
+    def prioritize_hacktoberfest_repos(self, repositories):
+        hacktoberfest_repos = [repo for repo in repositories if 'hacktoberfest' in repo['topics']]
+        other_repos = [repo for repo in repositories if 'hacktoberfest' not in repo['topics']]
+        # print(f'hacktoberfest repos: {hacktoberfest_repos}')
+        # print(f'other repos: {other_repos}')
+        return hacktoberfest_repos + other_repos
+
+
+    def get_latest_contributors(self, repositories):
+        """Get a list of latest contributors."""
+        
+        calc_12_hours = (datetime.now() - timedelta(hours=12)).strftime("%Y-%m-%dT%H:%M:%SZ")
+        repos_last_12_hours = [repo for repo in repositories if repo['updated_at'] < calc_12_hours
+        ]
+        latest_commits_url_each_repo = [repo['commits_url'].split('{')[0] for repo in repos_last_12_hours]
+
+        latest_commit_authors = []
+        latest_repo_names = []
+        
+        for url in latest_commits_url_each_repo:
+            commits_response = requests.get(url)
+            commits_response_json = commits_response.json()
+            
+            try:
+                if commits_response_json:
+                    latest_commit_author = commits_response_json[0].get('author', {}).get('login')
+                    if latest_commit_author:
+                        latest_commit_authors.append(latest_commit_author)
+            except KeyError:
+                print('empty url', commits_response_json)
+                continue
+            repo_name = url.split('/')[-2]
+            latest_repo_names.append(repo_name)
+        return zip(latest_commit_authors, latest_repo_names)
+
+
+# If performance becomes an issue, consider alternative methods to achieve randomness, such as selecting a random index in Python and retrieving the specific entry by ID.
+
 
 class GithubRepo(models.Model):
     name = models.CharField(max_length=255)
@@ -67,22 +101,12 @@ class GithubRepo(models.Model):
     stargazers_count = models.IntegerField()
     latest_commit_timestamp = models.DateTimeField(default=timezone.now, null=True, blank=True)
     latest_committer = models.CharField(max_length=255, null=True, blank=True)
-    
+
     objects = GithubRepoManager()
-    
+
     def __str__(self):
         return self.name
-    
-    def latest_contributors(self):
-        headers = {'Authorization': f'Bearer {env('GITHUB_ORG_ACCESS_TOKEN')}'}
-        response = requests.get(f'https://api.github.com/repos/{self.name}/commits', headers=headers)
-        response.raise_for_status()
-        commits = response.json()
-        if commits:
-            self.latest_committer = commits[0]['commit']['committer']['name']
-            self.save()
 
-# does views latest_contributors logic need to go here?
 class RepoContributor(models.Model):
     name = models.CharField(max_length=255)
     repo = models.ForeignKey(GithubRepo, on_delete=models.CASCADE)
--- osd_dashboard_app/views.py
+++ osd_dashboard_app/views.py
@@ -1,22 +1,31 @@
-from django.views.generic import TemplateView
+from django.views import View
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 from .models import GithubRepo
-import environ
+from .serializers import GithubRepoSerializer
 
+class GitHubRepositoriesView(View):
+    """A class-based view for retrieving GitHub repositories."""
 
-env = environ.Env(DEBUG=(bool, False))
-environ.Env.read_env(raise_error_if_not_found=True)
+    def get(self, request):
+        repositories = GithubRepo.objects.fetch_repos()
+        if not repositories:
+            return HttpResponse("Failed to fetch GitHub repositories", status=500)
 
-class GitHubRepositoriesView(TemplateView):
-    template_name = 'repositories.html'
-  
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        all_repos = GithubRepo.objects.fetch_repos()
-        
-        context['popular_repos'] = GithubRepo.objects.get_popular_repos(all_repos)
-        context['featured_repo'] = GithubRepo.objects.get_featured_repo(all_repos)
-        context['hacktoberfest_repos'] = GithubRepo.objects.prioritize_hacktoberfest_repos(all_repos)
-        
-        return context
+        repositories = GithubRepo.objects.prioritize_hacktoberfest_repos(repositories)
+        popular_repo_result = GithubRepo.objects.get_popular_repos(repositories)
+        featured_repo_result = GithubRepo.objects.get_featured_repo(popular_repo_result)
+        latest_contributors_result = GithubRepo.objects.get_latest_contributors(repositories)
+
+        context = {
+            "popular_repos_result": popular_repo_result,
+            "featured_repo_result": featured_repo_result,
+            "latest_contributors_result": latest_contributors_result,
+            "repositories": repositories,
+        }
+        return render(request, 'repositories.html', context)
     
-    
+    def github_repositories(request):
+        repositories = GithubRepo.objects.all()
+        serializer = GithubRepoSerializer(repositories, many=True)
+        return JsonResponse(serializer.data, safe=False)
