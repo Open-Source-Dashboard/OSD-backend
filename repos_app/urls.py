b01fbddd75df8a123026dbc20b2f065876cff21c
Andrea Riley <andreariley@me.com>
2024-05-20 17:04:36 -0400

    urls updated

--- repos_app/urls.py
+++ repos_app/urls.py
@@ -4,8 +4,8 @@ from .auth import GitHubAuthCallback
 
 
 urlpatterns = [
-  path('', GitHubRepositoriesView.as_view()),
-  path('github/callback/', GitHubAuthCallback.as_view(), name='github_callback'),
-  path('api/github/repositories/', GitHubRepositoriesView.as_view(), name='github_repositories')
-  path('check_user_commits/', GitHubRepositoriesView.as_view({'get': 'check_user_commits'}), name='check_user_commits')
+    path("", GitHubRepositoriesView.as_view()),
+    path("github/callback/", GitHubAuthCallback.as_view(), name="github_callback"),
+    path("api/github/repositories/", GitHubRepositoriesView.as_view(),name="github_repositories"),
+    path("check_user_commits/", GitHubRepositoriesView.as_view({"get": "check_user_commits"}),name="check_user_commits"),
 ]
