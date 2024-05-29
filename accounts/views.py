96dea0c8e21dc9e387409f71d298b3f9e9b7c024
Andrea Riley <andreariley@me.com>
2024-05-29 17:21:07 -0400

    fake data testing complete

--- accounts/views.py
+++ accounts/views.py
@@ -28,11 +28,16 @@ class UserDetailView(generics.RetrieveAPIView):
         serializer = self.get_serializer(user)
         user_data = serializer.data
         print('user data:', user_data)
-        user_events = user.fetch_user_push_events(user.user_name) 
-        print('user events:', user_events)
-        after_registration_events = user.events_after_registration(user.registration_date, user_events)
+        try:
+            user_events = GitHubUser.objects.fetch_user_push_events(user.user_name) 
+            print('user events:', user_events)
+        except requests.RequestException as e:
+            return JsonResponse({'error': str(e)}, status=500)
+        
+        after_registration_events = GitHubUser.objects.events_after_registration(user_events)
         print('after registration events:', after_registration_events)
-        user_commits = user.get_commits_from_push(after_registration_events)
+        
+        user_commits = GitHubUser.objects.get_commits_from_push(after_registration_events)
         print('User commits:', user_commits)
         
         commit_data = {
