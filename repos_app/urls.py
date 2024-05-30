e809658799df2a1edc2eaa993dff03b9a86a8a0d
tammytdo11 <tammytdo11@gmail.com>
2024-05-30 01:15:03 -0700

    remove comment

--- repos_app/urls.py
+++ repos_app/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
 from repos_app.views import GitHubRepositoriesView
-# from .auth import GitHubAuthCallback
-
 
 urlpatterns = [
     path("", GitHubRepositoriesView.as_view()),
