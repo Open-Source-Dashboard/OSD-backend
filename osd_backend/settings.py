a14db15ee42e83f9070bd964412e0a5c899c30a9
tammytdo11 <tammytdo11@gmail.com>
2024-07-02 02:00:50 -0700

    change origins

--- osd_backend/settings.py
+++ osd_backend/settings.py
@@ -109,6 +109,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-CORS_ALLOWED_ORIGINS = env.list('ALLOWED_ORIGINS')
+CORS_ALLOWED_ORIGINS = []
 CORS_ALLOW_ALL_ORIGINS = env.bool('ALLOW_ALL_ORIGINS')
-CSRF_TRUSTED_ORIGINS = env.list('ALLOWED_ORIGINS')
+CSRF_TRUSTED_ORIGINS = []
