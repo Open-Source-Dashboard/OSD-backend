55cfb83c4d08875c414967f250b7608df43c55f1
tammytdo11 <tammytdo11@gmail.com>
2024-07-01 18:56:58 -0700

    update vercel file

--- osd_backend/wsgi.py
+++ osd_backend/wsgi.py
@@ -1,18 +1,19 @@
 """
-WSGI config for osd_backend project.
+WSGI config for project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
+
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'osd_backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
 application = get_wsgi_application()
 
-#Vercel 
-app=application
+# Vercel
+app = application
