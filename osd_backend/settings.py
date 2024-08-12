5dc39f783acf34f4a8f3d17570b286f78fc4dcc7
tammytdo11 <tammytdo11@gmail.com>
2024-08-12 12:14:06 -0700

    rename app to application in wsgi.py

--- osd_backend/settings.py
+++ osd_backend/settings.py
@@ -70,7 +70,7 @@ WSGI_APPLICATION = 'osd_backend.wsgi.app'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': env.str('DB_NAME'),
         'USER': env.str('DB_USER'),
         'PASSWORD': env.str('DB_PASSWORD'),
