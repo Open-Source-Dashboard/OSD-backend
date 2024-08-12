d3eace52f0d62e318dbc975529235aa30613cb04
tammytdo11 <tammytdo11@gmail.com>
2024-08-12 02:11:06 -0700

    add supabase to requirements.txt

--- osd_backend/settings.py
+++ osd_backend/settings.py
@@ -19,7 +19,6 @@ SECRET_KEY = env.str("SECRET_KEY", default="your_default_secret_key")
 DEBUG = env.bool("DEBUG", default=False)
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=['localhost', '127.0.0.1', '.netlify.app', '.vercel.app'])
-print("ALLOWED_HOSTS:", ALLOWED_HOSTS) 
 
 # Application definition
 INSTALLED_APPS = [
